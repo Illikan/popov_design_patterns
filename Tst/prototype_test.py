70dbe34161787c921860c0bc5520cce3575d75e3
Попов Кирилл <morganerrat@gmail.com>
2024-05-19 23:40:20 +0800

    console logging

--- Tst/prototype_test.py
+++ Tst/prototype_test.py
@@ -4,7 +4,7 @@ from Src.settings_manager import settings_manager
 from Src.Storage.storage import storage
 from Src.exceptions import operation_exception
 from Src.Logics.Services.log_service import log_service
-
+from Src.Logics.Services.console_log_service import console_log_service
 from datetime import datetime
 import unittest
 
@@ -13,7 +13,7 @@ class prototype_test(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
         log_service()
-
+        console_log_service()
     #
     # Проверить метод filter_by_period
     #
